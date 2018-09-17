0bf1aaa14be3ddc5afe233ab64f3d894d237158f
yukminnie <dadiaomin@126.com>
2018-09-17 19:03:16 +0800

    canvas绘图简单实现

--- js/main.js
+++ js/main.js
@@ -2,5 +2,34 @@ var yyy = document.getElementById('xxx');
 
 var content = yyy.getContext('2d');
 
-content.fillStyle = 'black';
-content.fillRect(10,10, 100, 100);
+// content.fillStyle = 'black';
+// content.fillRect(10,10, 100, 100);
+
+function drawCircle(x, y, radius){
+  content.beginPath()
+  content.arc(x, y, radius, 0, 180)
+  content.stroke()
+}
+
+var painting = false
+
+yyy.onmousedown = function(a){
+  painting = true
+  var x = a.clientX
+  var y = a.clientY
+  drawCircle(x, y, 5)
+}
+
+yyy.onmousemove = function(a){
+  if (painting){
+      var x = a.clientX
+      var y = a.clientY
+      drawCircle(x, y, 5)
+  }else{
+
+  }
+}
+
+yyy.onmouseup = function(b){
+  painting = false
+}
