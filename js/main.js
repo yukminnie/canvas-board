5a24723707add27719d1f2f7f835fbbf93b0f7c9
yukminnie <dadiaomin@126.com>
2018-09-17 20:29:39 +0800

    画线实现(注意过去点的定义方法)

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var content = yyy.getContext('2d');
 // content.fillStyle = 'black';
 // content.fillRect(10,10, 100, 100);
 
+// 画圆
 function drawCircle(x, y, radius){
   content.beginPath()
   content.arc(x, y, radius, 0, 180)
@@ -12,20 +13,36 @@ function drawCircle(x, y, radius){
   content.fill()
 }
 
+// 画线
+function drawLine(x1, y1, x2, y2){
+  content.beginPath();
+  content.moveTo(x1, y1)
+  content.lineWidth = 5
+  content.lineTo(x2, y2)
+  content.stroke()
+  content.closePath()
+}
+
 var painting = false
+var lastPoint = {x: undefined, y: undefined}
 
 yyy.onmousedown = function(a){
   painting = true
   var x = a.clientX
   var y = a.clientY
-  drawCircle(x, y, 1)
+  lastPoint = {'x': x, 'y': y}
+  // drawCircle(x, y, 1)
+  
 }
 
 yyy.onmousemove = function(a){
   if (painting){
       var x = a.clientX
       var y = a.clientY
-      drawCircle(x, y, 1)
+      // drawCircle(x, y, 1)
+      var newPoint = {'x': x, 'y': y}
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
   }else{
 
   }
