b4447fac9adcc3f0a04dffde35c121f5f6d1d9ee
yukminnie <dadiaomin@126.com>
2018-09-18 08:02:30 +0800

    转换tab为4

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ yyy.height = pageHeight
 
 
 // 防止拉伸页面改变
-window.onresize = function () {
-  var pageWidth = document.documentElement.clientWidth
-  var pageHeight = document.documentElement.clientHeight
+window.onresize = function() {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
 
-  yyy.width = pageWidth
-  yyy.height = pageHeight
+    yyy.width = pageWidth
+    yyy.height = pageHeight
 }
 
 var content = yyy.getContext('2d');
@@ -24,50 +24,50 @@ var content = yyy.getContext('2d');
 
 // 画圆
 function drawCircle(x, y, radius) {
-  content.beginPath()
-  content.arc(x, y, radius, 0, 180)
-  // content.stroke()
-  content.fill()
+    content.beginPath()
+    content.arc(x, y, radius, 0, 180)
+    // content.stroke()
+    content.fill()
 }
 
 // 画线
 function drawLine(x1, y1, x2, y2) {
-  content.beginPath();
-  content.moveTo(x1, y1)
-  content.lineWidth = 5
-  content.lineTo(x2, y2)
-  content.stroke()
-  content.closePath()
+    content.beginPath();
+    content.moveTo(x1, y1)
+    content.lineWidth = 5
+    content.lineTo(x2, y2)
+    content.stroke()
+    content.closePath()
 }
 
 var using = false
 var lastPoint = {
-  x: undefined,
-  y: undefined
+    x: undefined,
+    y: undefined
 }
 
-yyy.onmousedown = function (a) {
-  var x = a.clientX
-  var y = a.clientY
-  if (eraserEnabled) {
-    using = true
-    context.clearRect(x-5, y-5, 10, 10)
-  } else {
-    using = true
-    lastPoint = {
-        'x': x,
-        'y': y
-  }
-   // drawCircle(x, y, 1)
-  }
+yyy.onmousedown = function(a) {
+    var x = a.clientX
+    var y = a.clientY
+    if (eraserEnabled) {
+        using = true
+        context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+        using = true
+        lastPoint = {
+            'x': x,
+            'y': y
+        }
+        // drawCircle(x, y, 1)
+    }
 }
 
-yyy.onmousemove = function (a) {
+yyy.onmousemove = function(a) {
     var x = a.clientX
     var y = a.clientY
     if (eraserEnabled) {
         if (using) {
-            content.clearRect(x-5, y-5, 10, 10)
+            content.clearRect(x - 5, y - 5, 10, 10)
         }
     } else {
         if (using) {
@@ -83,16 +83,16 @@ yyy.onmousemove = function (a) {
 }
 
 
-yyy.onmouseup = function (b) {
-  using = false
+yyy.onmouseup = function(b) {
+    using = false
 }
 
 var eraserEnabled = false
-eraser.onclick = function () {
-  eraserEnabled = !eraserEnabled
-  if (eraserEnabled) {
-      eraser.textContent = '画笔'
-  } else {
-      eraser.textContent = '橡皮檫'
-  }
+eraser.onclick = function() {
+    eraserEnabled = !eraserEnabled
+    if (eraserEnabled) {
+        eraser.textContent = '画笔'
+    } else {
+        eraser.textContent = '橡皮檫'
+    }
 }
